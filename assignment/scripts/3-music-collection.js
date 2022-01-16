0509d50dc7e903cdc2a6242d7b7bb923ff112371
Clay Webb <clay_webb@outlook.com>
2022-01-16 16:57:59 -0600

    Tested findByArtist function successfully, cleaned up structure

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,13 +3,13 @@ console.log('***** Music Collection *****');
 let collection = []; // Variable that starts as an empty array
 
 function addToCollection(title, artist, yearPublished) { // Start addToCollection function
-  console.log('In addToCollection function');
+  console.log('*** In addToCollection function ***');
   const album = {}; // Created the object album
     album.title = title;
     album.artist = artist;
     album.yearPublished = yearPublished;
 
-  collection.push(album.title, album.artist, album.yearPublished); // This pushes the parameters into the empty collection array
+  collection.push(album); // This pushes the parameters into the empty collection array
 
   return album;
 
@@ -22,7 +22,6 @@ const myList3 = addToCollection( 'All The Small Things', 'Blink-182', 2000);
 const myList4 = addToCollection( 'First Date', 'Blink-182', 2001);
 const myList5 = addToCollection( 'Put Your Records On', 'Ritt Momney', 2020); // Adding addToCollection parameter values
 
-collection.push(myList, myList1, myList2, myList3, myList4, myList5); // This pushes the new albums values into the collection array
 
 console.log(myList); // This will test and log the values for myList
 console.log(myList1); // This will test and log the values for myList1
@@ -34,7 +33,7 @@ console.log(myList5); // This will test and log the values for myList5
 console.log(collection); // This will log the values in the collection array
 
 function showCollection(array) { // Start showCollection function
-  console.log('In showCollection function');
+  console.log('*** In showCollection function ***');
   console.log(`There are ${array.length} items in the array`); // This will log the amount of items in the array
   for (let i of array) { // Start for of loop
     console.log(`${i.title} by ${i.artist}, published in ${i.yearPublished}`); // This will log each lists title, artist, and yearPublished when called
@@ -44,19 +43,22 @@ function showCollection(array) { // Start showCollection function
 showCollection(collection); // Calling the showCollection function with the collection array
 
 function findByArtist(artist) { // Start findByArtist function
-  console.log('In findByArtist function');
+  console.log('*** In findByArtist function ***');
   let artistList = [];
   for (let i = 0; i < collection.length; i++) {
     if (collection[i].artist === artist) {
       artistList.push(collection[i].artist);
-    }
-    else {
-      console.log('Artist not available');
-    }
-  }
+      console.log('Yes!'); // This will be logged if we find an artist in our array
+    } // End if statement
+  } // End for loop
 
   console.log(artistList);
 } // End findByArtist function
+console.log('Is there any Blink-182?');
+findByArtist('Blink-182'); // This will log that there are two songs by Blink-182
 
-findByArtist('Blink-182');
-findByArtist('Michael Jackson');
+console.log('Is there any Michael Jackson?');
+findByArtist('Michael Jackson'); // This will log that there are no Michael Jackson songs
+
+console.log('Is there any Nic D?');
+findByArtist('Nic D'); // This will log that there is one song by Nic D
