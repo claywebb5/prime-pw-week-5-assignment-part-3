b5e793b05ef8963b525ed2f0571c5ae3bbf143c3
Clay Webb <clay_webb@outlook.com>
2022-01-16 14:23:38 -0600

    Added code comments for the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,34 +1,31 @@
 console.log('***** Music Collection *****');
 
-let collection = [];
+let collection = []; // Variable that starts as an empty array
 
-function addToCollection(title, artist, yearPublished) {
-  const album = {};
+function addToCollection(title, artist, yearPublished) { // Start addToCollection function
+  const album = {}; // Created the object album
     album.title = title,
     album.artist = artist,
     album.yearPublished = yearPublished
 
-  collection.push(album.title, album.artist, album.yearPublished);
+  collection.push(album.title, album.artist, album.yearPublished); // This pushes the parameters into the empty collection array
 
   return album;
 
-}
+} // End addToCollection function
 
 const myList = addToCollection( 'Sunshine', 'OneRepublic', 2021);
 const myList1 = addToCollection( 'Mr. Brightside', 'The Killers', 2003);
 const myList2 = addToCollection( 'Fine Apple', 'Nic D', 2021);
 const myList3 = addToCollection( 'All The Small Things', 'Blink-182', 2000);
 const myList4 = addToCollection( 'First Date', 'Blink-182', 2001);
-const myList5 = addToCollection( 'Put Your Records On', 'Ritt Momney', 2020);
+const myList5 = addToCollection( 'Put Your Records On', 'Ritt Momney', 2020); // Adding addToCollection parameter values
 
-collection.push(myList, myList1, myList2, myList3, myList4, myList5);
+collection.push(myList, myList1, myList2, myList3, myList4, myList5); // This pushes the new albums values into the collection array
 
-console.log(myList);
-console.log(myList1);
-console.log(myList2);
-console.log(myList3);
-console.log(myList4);
-console.log(myList5);
-
-console.log(myList.title);
-console.log(collection[0], collection[1], collection[2]);
+console.log(myList); // This will log the values for myList
+console.log(myList1); // This will log the values for myList1
+console.log(myList2); // This will log the values for myList2
+console.log(myList3); // This will log the values for myList3
+console.log(myList4); // This will log the values for myList4
+console.log(myList5); // This will log the values for myList5
